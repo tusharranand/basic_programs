e1542bdc8d5b825eb021b7fd83d7365743fd4e1d
tusharranand <tusharranand@gmail.com>
2022-03-05 14:24:29 +0530

    Flips a coin

---
+++ coin_flip/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// to initiate an integer variable 
+int coin_head = 1;
+// to make a basic object named "x"
+Random x = new Random();
+int coin_check = x.Next(0, 2);
+
+Console.WriteLine("Random number was: " + coin_check);
+
+if (coin_check == coin_head)
+{
+    Console.WriteLine("Coin landed on Heads.");
+}
+else
+{
+    Console.WriteLine("Coin landed on Tails.");
+}
