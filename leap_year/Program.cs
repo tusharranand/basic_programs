3b358b9f9c4c7faeb3b077e50468716d19287eef
tusharranand <tusharranand@gmail.com>
2022-03-05 14:12:44 +0530

    Checks for leap year

---
+++ leap_year/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.WriteLine("Enter the year: ");
+int year = int. Parse(Console.ReadLine());
+
+// && -> is used for "and"
+// || -> is used for "or"
+
+//if (((year%4 == 0) && (year%100 != 0)) || (year%400 == 0))
+//{
+//    Console.WriteLine("{0} is a leap year", year);
+//}
+//else
+//{
+//    Console.WriteLine(year + " is not a leap year");
+//}
+
+// internally given method by visual studio for leap year
+
+if (DateTime.IsLeapYear(year))
+{
+    Console.WriteLine("{0} is a leap year", year);
+}
+else
+{
+    Console.WriteLine(year + " is not a leap year");
+}
